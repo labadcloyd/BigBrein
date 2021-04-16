7f4ed22229f8aa3b3e8e06db799cc30a94ca03dc
labadcloyd <abadcloyd@gmail.com>
2021-04-16 08:14:51 +0800

    added username duplicate checker and password confirmation

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,60 +1,101 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import classes from './auth-form.module.css';
 import axios from 'axios';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-
-  function switchAuthModeHandler() {
-    setIsLogin((prevState) => !prevState);
-  }
-
-  async function createUser(username, password){
-	try{
-		await axios.post('/api/auth/signup', {username,password})
-	} catch(error) {
-		console.error(error);
+	/* for switching to login or to signup */
+	const [isLogin, setIsLogin] = useState(true);
+	const [credentials, setCredentials] = useState({})
+	const [confirmPassword, setConfirmation] = useState()
+	const [isConfirmationError, setConfirmationError] = useState()
+	/* for showing any errors when submitting data */
+	const [isError, setError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+	
+	/* for showing any errors when submitting data */
+	async function handleChange(event){
+		const {id, value} = event.target
+		setError('')
+		setCredentials((prevInput)=>{
+			return({
+				...prevInput,
+				[id]:value
+			})
+		})
 	}
-  }
-  async function submitHandler(event){
-	event.preventDefault()
-	const enteredUsername = usernameRef.current.value;
-	const enteredPassword = passwordRef.current.value;
-
-	if(isLogin){
-
+	/* for confirming password */
+	async function handleConfirmation(event){
+		const {value} = event.target
+		setConfirmation(value)
+		setConfirmationError(false)
 	}
-	else if (!isLogin){
-		await createUser(enteredUsername, enteredPassword)
+	/* function for submiting user credentials */
+	async function createUser(username, password){
+		try{
+			const response = await axios.post('/api/auth/signup', {username,password})
+			return response
+		}catch(error){
+			/* You can only retrieve the json of the res error through adding .response at the end */
+			return error.response
+		}
+  	}
+	/* Sending the user credentials to the database */
+	async function submitHandler(event){
+		event.preventDefault()
+		/* For Login */
+		if(isLogin){
+
+		}
+		/* For Signup */
+		else if (!isLogin){
+			if(confirmPassword !== credentials.password){
+				return setConfirmationError(true)
+			} else{
+				setConfirmationError(false)
+			}
+			const response = await createUser(credentials.username, credentials.password)
+			const data = await response
+			if(data.status===422){
+				setError(true)
+				setErrorMessage(data.data.message)
+			}
+		}
+  	}
+	function switchAuthModeHandler() {
+		setIsLogin((prevState) => !prevState);
 	}
-  }
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor='username'>Your Username</label>
-          <input type='text' id='username' required ref={usernameRef}/>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passwordRef} />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+  	return (
+		<section className={classes.auth}>
+			
+			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<div style={{color:'red'}}> {isError ? [errorMessage] : ''}</div>
+					<label htmlFor='username'>Your Username</label>
+					<input type='text' id='username' required value={credentials.username} onChange={handleChange} />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor='password'>Your Password</label>
+					<input type='password' id='password' required value={credentials.password} onChange={handleChange} />
+				</div>
+				<div className={classes.control} style={ {display:isLogin ? 'none': 'block'}}>
+					<div style={{color:'red'}}> {isConfirmationError ? 'Password does not match' : ''}</div>
+					<label htmlFor='password'>Confirm Your Password</label>
+					<input type='password' id='confirmPassword' required value={confirmPassword} onChange={handleConfirmation}/>
+				</div>
+				<div className={classes.actions}>
+					<button>{isLogin ? 'Login' : 'Create Account'}</button>
+					<button
+						type='button'
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}
+					>
+					{isLogin ? 'Create new account' : 'Login with existing account'}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 }
 
 export default AuthForm;
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -5,17 +5,23 @@ export default async function handler(req, res){
 	if(req.method ==='POST'){
 		const data = req.body
 		const {username, password} = data
-		console.log(username,password)
 
+		/* basic validation for inputs */
 		if(!username ||!password || password.trim().length < 7 ){
-			res.status(422).json({message:'invalid input- password should also be at least 7 characters long.'})
-			return;
+			return res.status(422).json({message:'invalid input- password should also be at least 7 characters long.'})
 		}
+		/* checking for duplicate username */
+		const duplicateUser = await User.findOne({username:username},(err, duplicateUser)=>{
+			if(duplicateUser){return duplicateUser} else if(!duplicateUser){return null}
+		})
+		if (duplicateUser){
+			return res.status(422).json({message:'Username already taken.'})
+		}
+		/* hashing password and registering user */
 		const hashedPassword = await hashPassword(password)
 		try{
 			const user = {username:username, password: hashedPassword}
 			await User.insertMany(user)
-			console.log(hashedPassword)
 			return res.status(201).json({message:'Successfully signed up'})
 		}
 		catch (error) {
