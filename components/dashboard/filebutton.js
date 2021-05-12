1f6c473e93a492373e9413138c3f9c58474bfd6e
labadcloyd <abadcloyd@gmail.com>
2021-05-12 01:10:19 +0000

    added click away detector for file button

--- components/dashboard/filebutton.js
+++ components/dashboard/filebutton.js
@@ -1,12 +1,17 @@
+import { ClickAwayListener } from '@material-ui/core';
 import {MoreVert, Delete, Edit } from '@material-ui/icons'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import css from './filebutton.module.css'
 
 export default function Filebutton(props){
 	const {flashcardIcon, noteIcon, quizIcon, title, filetype, fileID, fileFolderID, index} = props
-	const [showOptions, toggleOption] = useState(false)
 	function handleDelete(){
-
+		
+	}
+	/* for showing or hiding file button */
+	const [showOptions, toggleOption] = useState(false)
+	function handleClickAway(){
+		toggleOption(false)
 	}
 	function handleOptionClick(){
 		toggleOption((prevValue)=>{
@@ -14,23 +19,25 @@ export default function Filebutton(props){
 		})
 	}
 	return(
-		<div className={css.fileButtonWrapper}>
-			<a key={index} className={css.fileContainer} href={`/files/${filetype}/${fileID}/${fileFolderID}`}>
-				<span>{title}</span>
-				<div>
-					{filetype==='flashcard'?<>{flashcardIcon}</>:<></>}
-					{filetype==='note'?<>{noteIcon}</>:<></>}
-					{filetype==='quiz'?<>{quizIcon}</>:<></>}
-					{filetype}
-				</div>
-			</a>
-			<div className={css.fileOptionWrapper}>
-				<a onClick={handleOptionClick} ><MoreVert/></a>
-				<div className={css.fileOptionContainer} style={{display:showOptions?'flex':'none'}}>
-					<a href={`/files/${filetype}/edit${filetype}/${fileID}/${fileFolderID}`}><Edit/>Edit</a>
-					<a onClick={handleDelete}><Delete/>Delete</a>
+		<ClickAwayListener onClickAway={handleClickAway}>
+			<div className={css.fileButtonWrapper} >
+				<a key={index} className={css.fileContainer} href={`/files/${filetype}/${fileID}/${fileFolderID}`}>
+					<span>{title}</span>
+					<div>
+						{filetype==='flashcard'?<>{flashcardIcon}</>:<></>}
+						{filetype==='note'?<>{noteIcon}</>:<></>}
+						{filetype==='quiz'?<>{quizIcon}</>:<></>}
+						{filetype}
+					</div>
+				</a>
+				<div className={css.fileOptionWrapper}>
+					<a onClick={handleOptionClick} ><MoreVert/></a>
+					<div className={css.fileOptionContainer} style={{display:showOptions?'flex':'none'}}>
+						<a href={`/files/${filetype}/edit${filetype}/${fileID}/${fileFolderID}`}><Edit/>Edit</a>
+						<a onClick={handleDelete}><Delete/>Delete</a>
+					</div>
 				</div>
 			</div>
-		</div>
+		</ClickAwayListener>
 	)
 }
